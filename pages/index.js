27b3308d790d74900791d80720579d278508b110
Konrad Głowacki <konrad.glowacki@wpengine.com>
2020-11-10 19:45:35 +0100

    Change index

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">WP Engine!</a>
+          Only Build
         </h1>
 
         <p className="description">
